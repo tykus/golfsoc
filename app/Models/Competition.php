1f354a3e6a795f28138f469efb6bf5d7f7819d5b
Brian O'Sullivan <brian@tykus.ie>
2025-02-26 14:05:48 +0000

    Add model, migration, seeders for the Competition model

---
+++ app/Models/Competition.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Competition extends Model
+{
+    /** @use HasFactory<\Database\Factories\CompetitionFactory> */
+    use HasFactory;
+}
