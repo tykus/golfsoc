aecb20041e32178616fa5a47da2a1e3522fe9581
Brian O'Sullivan <brian@tykus.ie>
2025-02-26 13:01:43 +0000

    Add `holes` relationship on Course

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseFactory> */
     use HasFactory;
+
+    public function holes(): HasMany
+    {
+        return $this->hasMany(Hole::class);
+    }
 }
